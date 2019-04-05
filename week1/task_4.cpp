366904f8bf6d73f98e2bea50c7f9bbeae39e5f94
Zubaer Imran <zubaer.imran07cse@gmail.com>
2019-04-05 15:03:16 -0700

    what will be the output?

---
+++ week1/task_4.cpp
@@ -0,0 +1,13 @@
+//Will it compile? If so, what will be printed?
+
+#include <iostream>
+using namespace std;
+  
+int main () {
+	int numerator = 5;
+	int divisor = 2;
+	int result = numerator / divisor;
+	cout << result << endl;
+
+  return 0;
+}
